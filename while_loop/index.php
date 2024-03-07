c7164fa5a3e1a701a4fe2631006870b9989628cb
Peppe <141502459+Peppe2404@users.noreply.github.com>
2024-03-07 17:05:22 +0100

    Add files via upload

---
+++ while_loop/index.php
@@ -0,0 +1,11 @@
+<?php
+/*a while loop repeat some code infinitely 
+while some condition remains true*/
+
+    $counter = 0; //inizio
+    
+    while($counter < 15){ //continua fino a 
+        $counter++; //incrementando di 1
+        echo $counter . "<br>";
+    }
+?>

c80605b95359ade428c0891ba91c0b71a89a03e0
Peppe <141502459+Peppe2404@users.noreply.github.com>
2024-03-07 16:52:13 +0100

    Add files via upload

---
+++ basic_arithmetic/index.php
@@ -0,0 +1,43 @@
+<?php
+
+// Arithmetic operators
+    // + - * / ** %
+
+    $x = 10;
+    $y = 2;
+    $z = null;
+
+    // $z = $x + $y;
+    //$z = $x - $y;
+    //$z = $x * $y;
+    //$z = $x / $y;
+    //$z = $x ** $y;
+    //$z = $x - $y;
+    //echo $z;
+
+// Increment/Decrement operators
+    // ++ -- += -=
+
+    $counter = 2;
+
+    //$counter++; incrementa il valore di 1
+    //$counter--; decrementa il valore di 1
+    //$counter+=5; incrementa il valore di 5
+    //$counter-=5; //decrementa il valore di 5
+    //echo $counter;
+
+// Operator Precedence
+    // ()
+    // **
+    // * / %
+    // + -
+
+    $total = 1 + 2 - 3 * 4 / 5 ** 6;
+    echo $total;
+
+
+
+
+
+
+?>
